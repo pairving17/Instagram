78481e3fe6148bef0aba7d5fb79e1361cb04287b
pairving17 <pairving17.students.desu.edu>
2019-03-19 23:54:38 -0400

    Update 3/19/19

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -25,6 +25,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://guarded-cliffs-41045.herokuapp.com/parse"
             })
         )
+        if PFUser.current() != nil {
+           let main = UIStoryboard(name: "Main", bundle: nil)
+           let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+            window?.rootViewController = feedNavigationController
+        
+        }
         return true
     }
 
