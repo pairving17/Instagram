0c82370e0e26ec91c57c1e5b511411f17e5ca828
pairving17 <pairving17.students.desu.edu>
2019-03-19 23:52:30 -0400

    Update 3/19/19

---
+++ Instagram/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Instagram
+//
+//  Created by Philip Irivng  on 3/14/19.
+//  Copyright © 2019 Philip Irivng . All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
